f68913d6786e65eec5a862918989104a9dd80d77
Victor Villafranco <vitorvillafranco07@gmail.com>
2022-11-29 22:31:41 -0600

    Add file creation into HTML folder

--- html-clin.js
+++ html-clin.js
@@ -1,11 +1,19 @@
 const fs = require("fs");
-const path = require("path");
+const pathDir = require("path");
 
 //Método para crear directorios
-fs.mkdir(path.join(__dirname,'HTML'),(err) => {
+fs.mkdir(pathDir.join(__dirname,'HTML'),(err) => {
     if (err){
         return console.error("Can't be create the folder, because it has allready exists",err);
     } else {
         console.log("The folder has been created");
     }
+
+fs.writeFile("HTML/index.html","",{encoding:"utf-8"}, (err)=> {
+    if (err){
+        console.error("The file can't be created",err);
+    } else {
+        console.log("The file has been created successfully");
+    }
+});
 });
