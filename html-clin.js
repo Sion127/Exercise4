4ff79016b8463819fc3b7dcae1edd7748ba380f9
Victor Villafranco <vitorvillafranco07@gmail.com>
2022-11-29 21:14:59 -0600

    Creat a new folder in the proyect

--- html-clin.js
+++ html-clin.js
@@ -1,2 +1,12 @@
 const fs = require("fs");
-const process = require("process");
+const path = require("path");
+
+//Método para crear directorios
+fs.mkdir(path.join(__dirname,'HTML'), {recursive:true}, (err) => {
+    if (err){
+        console.log("Can't be create the folder");
+        console.error(err);
+    } else {
+        console.log("The folder has been created");
+    }
+});
