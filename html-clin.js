a6ca4a174058672ddf46caf45b194399ab55bba3
Victor Villafranco <vitorvillafranco07@gmail.com>
2022-11-29 23:42:24 -0600

    Update with the switch

--- html-clin.js
+++ html-clin.js
@@ -1,19 +1,31 @@
 const fs = require("fs");
-const pathDir = require("path");
+const process = require("process");
+let option;
+let pathDir = process.argv[2];
+let pathFile = pathDir +"/index.html";
+switch (option){
 
-//Método para crear directorios
-fs.mkdir(pathDir.join(__dirname,'HTML'),(err) => {
-    if (err){
-        return console.error("Can't be create the folder, because it has allready exists",err);
-    } else {
-        console.log("The folder has been created");
-    }
+    case "HTML":
 
-fs.writeFile("HTML/index.html","",{encoding:"utf-8"}, (err)=> {
-    if (err){
-        console.error("The file can't be created",err);
-    } else {
-        console.log("The file has been created successfully");
-    }
-});
-});
+    //Método para crear directorios
+        fs.mkdir(pathDir,(err) => {
+            if (err){
+                return console.error("Can't be create the folder, because it has allready exists",err);
+            } else {
+                console.log("The folder has been created");
+              }
+
+    //Metodo para crear el archivo
+        fs.writeFile(pathFile,"",{encoding:"utf-8"}, (err)=> {
+            if (err){
+                console.error("The file can't be created",err);
+            } else {
+                console.log("The file has been created successfully");
+              }
+        });
+    });
+
+break;
+
+
+}
