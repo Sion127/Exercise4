416a6b5a5ff02d3ec4284aa6e5cf8a3b8aa0c0bd
Victor Villafranco <vitorvillafranco07@gmail.com>
2022-11-29 21:38:10 -0600

    Update creart the folder HTML in the project

--- html-clin.js
+++ html-clin.js
@@ -2,10 +2,9 @@ const fs = require("fs");
 const path = require("path");
 
 //Método para crear directorios
-fs.mkdir(path.join(__dirname,'HTML'), {recursive:true}, (err) => {
+fs.mkdir(path.join(__dirname,'HTML'),(err) => {
     if (err){
-        console.log("Can't be create the folder");
-        console.error(err);
+        return console.error("Can't be create the folder, because it has allready exists",err);
     } else {
         console.log("The folder has been created");
     }
